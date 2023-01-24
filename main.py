7f44d5f0632182cba6041063c32f1d71af2e55de
wepeqoor1 <hoopeeeee@gmail.com>
2023-01-24 21:54:39 +0300

    Exception handling for download files

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import requests as requests
 from pathlib import Path
 
 
+def check_for_redirect(response: requests.Response) -> None:
+    response_url = response.url
+    print(response_url)
+    if response_url == 'https://tululu.org/' or response.history:
+        raise requests.HTTPError()
+
+
 def download_book(book_name: str, dir_name: str,  url: str) -> None:
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
 
     with open(f'{dir_name}/{book_name}.txt', 'wb') as file:
         file.write(response.content)
@@ -19,9 +27,12 @@ def main():
     dir_name = 'books'
     create_directory(dir_name)
 
-    for book_id in range(10, 20):
+    for book_id in range(1, 10):
         url = f'https://tululu.org/txt.php?id={book_id}/'
-        download_book(str(book_id), dir_name, url)
+        try:
+            download_book(str(book_id), dir_name, url)
+        except requests.HTTPError:
+            continue
 
 
 if __name__ == '__main__':
