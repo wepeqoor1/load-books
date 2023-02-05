7816bc223b1bb766373054b22d67749b911ddd58
wepeqoor1 <hoopeeeee@gmail.com>
2023-02-05 03:17:17 +0300

    Replace search from html to selector

--- download_books.py
+++ download_books.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import time
 from pathlib import Path
 from urllib.parse import urljoin, urlparse, urlsplit
@@ -7,7 +8,6 @@ import requests as requests
 from bs4 import BeautifulSoup
 from loguru import logger
 from pathvalidate import sanitize_filename
-from tqdm import tqdm
 
 
 def check_redirect(response: requests.Response) -> None:
@@ -38,7 +38,7 @@ def get_response(url: str) -> requests.Response:
     return response
 
 
-def download_txt(book_id: int, file_name: str, url: str, dir_name: str = 'books/') -> str:
+def download_txt(book_id: str, file_name: str, url: str, dir_name: str = 'books/') -> str:
     """Скачивает текст"""
     response = get_response(url)
     clear_file_name = sanitize_filename(file_name)
@@ -67,16 +67,16 @@ def parse_book_page(response: requests.Response) -> dict:
     """Парсит html страницу и возвращает данные о книге"""
     page_html = BeautifulSoup(response.text, 'lxml')
 
-    book_title = page_html.find('h1')
+    book_title = page_html.select_one('h1')
     title, author = [book.strip() for book in book_title.text.split('::')]
 
-    image_path = page_html.find('div', class_='bookimage').find('img')['src']
+    image_path = page_html.select_one('.bookimage img')['src']
     image_url = urljoin(f'https://{urlsplit(response.url).netloc}', image_path)
 
-    comments_html = page_html.find_all('div', class_='texts')
-    comments = [comment.find('span', class_='black').text for comment in comments_html] if comments_html else None
+    comments_html = page_html.select('.texts .black')
+    comments = [comment.text for comment in comments_html] if comments_html else None
 
-    genres_html = page_html.find('span', class_='d_book').find_all('a')
+    genres_html = page_html.select('span.d_book a')
     genres = [genre.text for genre in genres_html] if comments else None
 
     return {
@@ -104,35 +104,60 @@ def main() -> None:
 
     parser = create_parser()
     args = parser.parse_args()
-    book_id_from = args.first
-    book_id_last = args.last
+    category_page_num_from = args.first
+    category_page_num_to = args.last
+
+    category_url_param = 'l55/'
+    category_name = 'Научная фантастика.json'
+    url_path = 'https://tululu.org/'
+    category_url = urljoin(url_path, category_url_param)
 
     Path('books').mkdir(parents=True, exist_ok=True)
     Path('images').mkdir(parents=True, exist_ok=True)
 
-    for book_id in tqdm(range(book_id_from, book_id_last + 1)):
-        logger.log('BOOK', f'Книга с id {book_id}')
+    books_content = []
+    for num_page in range(category_page_num_from, category_page_num_to + 1):
+        logger.info(f'{num_page} страница категории')
+        category_page_url = urljoin(category_url, str(num_page))
+        response_category_page: requests.Response = get_response(category_page_url)
 
-        url = f'https://tululu.org/b{book_id}/'
-        try:
-            response = get_response(url)
-            book_content = parse_book_page(response)
-            logger.info(f'Информация о книге собрана с адреса {url}')
-        except requests.HTTPError or ValueError:
-            logger.warning(f'На странице {url} отсутствует книга. Переходим к следующей.')
-            continue
+        category_html = BeautifulSoup(response_category_page.text, 'lxml')
+        book_params = category_html.find_all(class_='d_book')
+        books_url = [urljoin(url_path, book.find('a').get('href')) for book in book_params]
 
-        try:
-            download_txt(book_id, book_content['title'], url)
-            logger.info(f'Книга скачена')
-        except requests.HTTPError:
-            logger.info(f'Книга на странице {url} на скачена. Текст отсутствует по данному адресу.')
+        for book_url in books_url:
+            print(book_url)
+            book_id = urlparse(book_url).path.strip('/').strip('b')
 
-        try:
-            download_image(book_content['image_url'])
-            logger.info(f'Изображение скачено')
-        except requests.HTTPError:
-            logger.info(f'Обложка на странице {url} на скачена. Картинка отсутствует по данному адресу.')
+            try:
+                response = get_response(book_url)
+                book_content = parse_book_page(response)
+                logger.info(f'Информация о книге собрана с адреса {book_url}')
+            except requests.HTTPError or ValueError:
+                logger.warning(f'На странице {book_url} отсутствует книга. Переходим к следующей.')
+                continue
+
+            try:
+                book_path = download_txt(book_id, book_content['title'], book_url)
+                book_content['book_path'] = book_path
+                logger.info(f'Книга скачена')
+            except requests.HTTPError:
+                logger.info(f'Книга на странице {book_url} на скачена. Текст отсутствует по данному адресу.')
+                continue
+
+            try:
+                img_src = download_image(book_content['image_url'])
+                book_content['img_src'] = img_src
+                logger.info(f'Изображение скачено')
+            except requests.HTTPError:
+                logger.info(f'Обложка на странице {book_url} на скачена. Картинка отсутствует по данному адресу.')
+
+            books_content.append(book_content)
+
+    books_content_json = json.dumps(books_content, ensure_ascii=False, indent=4)
+    with open(category_name, 'w', encoding='utf-8') as file:
+        file.write(books_content_json)
+    logger.info(f'Данные о книгах записаны в файл: "{category_name}"')
 
 
 if __name__ == '__main__':
