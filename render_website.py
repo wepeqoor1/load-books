a28dbe099747bfa3513a84fad16af99931839a65
wepeqoor1 <hoopeeeee@gmail.com>
2023-02-10 01:39:30 +0300

    Add frontend space

---
+++ render_website.py
@@ -0,0 +1,23 @@
+import json
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+with open('dest_folder/Научная фантастика.json', 'r', encoding='utf-8') as read_file:
+    books_content = json.load(read_file)
+    books_content = {'books_content': books_content}
+
+rendered_page = template.render(books_content)
+
+with open('index.html', 'w', encoding='utf8') as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
