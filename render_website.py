b642aca0d1a42d9b8a813270ed1fdb96b965aad8
wepeqoor1 <hoopeeeee@gmail.com>
2023-02-25 15:22:17 +0300

    Show to columns in main page

--- render_website.py
+++ render_website.py
@@ -1,17 +1,16 @@
 import json
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+import more_itertools
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
-
 template = env.get_template('template.html')
 
 with open('dest_folder/Научная фантастика.json', 'r', encoding='utf-8') as read_file:
-    books_content = json.load(read_file)
+    books_content = list(more_itertools.chunked(json.load(read_file), 2))
     books_content = {'books_content': books_content}
 
 rendered_page = template.render(books_content)
