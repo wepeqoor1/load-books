03f714b747adbcdc999b8b4f8066e6c0540c0bf2
wepeqoor1 <hoopeeeee@gmail.com>
2023-03-15 22:31:09 +0300

    Add link to page paginator

--- render_website.py
+++ render_website.py
@@ -1,7 +1,6 @@
 import json
-import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
+from pathlib import PurePath, Path, PurePosixPath
 
 import more_itertools
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -16,15 +15,19 @@ template = env.get_template('template.html')
 
 with open('dest_folder/Научная фантастика.json', 'r', encoding='utf-8') as read_file:
     books_content = list(more_itertools.chunked(json.load(read_file), 2))
-    books_content_chunk = list(more_itertools.chunked(books_content, 10))
+    books_content_chunked = list(more_itertools.chunked(books_content, 10))
 
 Path(PAGES_DIR).mkdir(parents=True, exist_ok=True)
 
-for num, books_content in enumerate(books_content_chunk):
-    html_page = f'index{num + 1}.html'
-    rendered_page = template.render({'books_content': books_content})
-    with open(Path(PAGES_DIR, html_page), 'w', encoding='utf8') as file:
+pages_path = [f'index{num + 1}.html' for num, _ in enumerate(books_content_chunked)]
+
+for page_content, page_path in zip(books_content_chunked, pages_path):
+    rendered_page = template.render({
+        'books_content': page_content,
+        'pages_path': pages_path
+    })
+    with open(Path(PAGES_DIR, page_path), 'w', encoding='utf8') as file:
         file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+# server.serve_forever()
